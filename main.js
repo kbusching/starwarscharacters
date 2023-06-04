4a38c180744000915170186775da2b70d64f264e
Katie Busching <katie.busching@gmail.com>
2023-06-04 15:54:39 -0400

    star wars database

---
+++ main.js
@@ -0,0 +1,32 @@
+let searchinput = document.getElementById('search');
+
+document.getElementById('get-character').addEventListener('click', function(){
+
+})
+
+document.getElementById('get-all-characters').addEventListener('click', function(){
+
+})
+
+searchinput.addEventListener('keyup', function(){
+
+});
+
+function fetchRandomCharacter(){
+    let randomID = Math.floor(Math.random() * 9) +1;
+    
+fetch:('https://swapi.dev/api/people/${randomID}/')
+.then(response => response.json());
+.then(data => {
+    document.getElementById('character').innerHTML =
+    `<h2>${data.name}</h2>;
+    <p>Height: ${data.height}cm</p>
+    <p>Mass: ${data.mass}kg</p>
+    <p>Hair color: ${data.hair_color}</p>
+    <p>Skin Color: ${data.skin_color}</p>
+    <p>Eye Color: ${data.eye_color}</p>
+    <p>Birth Year: ${data.birth_year}</p>
+    <p>Gender: ${data.gender}</p>`;
+})
+.catch(error => console.log('Error: ', error));
+}
